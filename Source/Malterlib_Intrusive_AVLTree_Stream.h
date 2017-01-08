7b5475da094eb813eb2c39cfba7e93f2454e6d69
Erik Olofsson <erik.olofsson@hansoft.com>
2017-01-08 14:40:56 +0000

    [Malterlib/Intrusive] Add support for moving containers into stream (integrated)
    Copied from Perforce
     Change: 81211

--- Source/Malterlib_Intrusive_AVLTree_Stream.h
+++ Source/Malterlib_Intrusive_AVLTree_Stream.h
@@ -36,6 +36,29 @@ namespace NMib
 				};
 			}
 	
+			static void fs_Feed(t_CStream &_Stream, NIntrusive::TCAVLTreeAggregate<t_CLinkTraits, t_CCompare, t_CAllocator> &&_Data)
+			{
+				typename NIntrusive::TCAVLTreeAggregate<t_CLinkTraits, t_CCompare, t_CAllocator>::template TIterator<> Iter(_Data);
+	
+				mint nItems = 0;
+	
+				while (Iter)
+				{
+					++nItems;
+					++Iter;
+				};
+	
+				fg_FeedLenToStream(_Stream, nItems);
+	
+				Iter = _Data;
+	
+				while (Iter)
+				{
+					_Stream.f_Feed(fg_Move(*Iter));
+					++Iter;
+				};
+			}
+	
 			static void fs_Consume(t_CStream &_Stream, NIntrusive::TCAVLTreeAggregate<t_CLinkTraits, t_CCompare, t_CAllocator> &_Data)
 			{
 				uint64 nItems;
@@ -73,6 +96,11 @@ namespace NMib
 			static void fs_Feed(t_CStream &_Stream, NIntrusive::TCAVLTree<t_CLinkTraits, t_CCompare, t_CAllocator> const &_Data)
 			{
 				_Stream << (const NIntrusive::TCAVLTreeAggregate<t_CLinkTraits, t_CCompare, t_CAllocator> &)_Data;
+			}
+
+			static void fs_Feed(t_CStream &_Stream, NIntrusive::TCAVLTree<t_CLinkTraits, t_CCompare, t_CAllocator> &&_Data)
+			{
+				_Stream << fg_Move((NIntrusive::TCAVLTreeAggregate<t_CLinkTraits, t_CCompare, t_CAllocator> &)_Data);
 			}
 	
 			static void fs_Consume(t_CStream &_Stream, NIntrusive::TCAVLTree<t_CLinkTraits, t_CCompare, t_CAllocator> &_Data)
